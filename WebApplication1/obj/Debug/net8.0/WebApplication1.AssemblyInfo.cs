14cbc15f519a81864b23c3273d2dd032ac42bc05
Sri5275 <saketh1844@gmail.com>
2024-04-15 10:54:07 +0530

    Installed Test Packages

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63b12d6a571f263711e21a4f644c086057bfecc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7175cfa78ccd2a1c7139fc6c350e5535a87042e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
